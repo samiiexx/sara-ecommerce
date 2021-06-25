560e50925204c32f63b7ea1e3b0523c5913477ae
Favour Samuel <sfchidimma@gmail.com>
2021-06-25 10:46:42 +0100

    Include main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId), nav = document.getElementById(navId)
+    const toggle = document.getElementById(toggleId),       nav = document.getElementById(navId)
 
     if(toggle && nav){
         toggle.addEventListener('click', ()=>{
